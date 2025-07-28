aae0e1713fc78c80628d0df1c79e54f4f7a8ebdd
nicolassm145 <d2022014472@unifei.edu.br>
2025-07-28 10:47:07 -0300

    Initial commit of the project structure and main functionality for adding friends.

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo(){
+    let input = document.getElementById("amigo");
+    let amigo = input.value.trim();   // Remove espaços em branco
+
+    // Validação se está vazio
+    if (amigo === "") {
+        alert("Por favor, insira um nome válido.");
+        return;
+    }
+
+    amigos.push(amigo); 
+
+    // Atualiza a lista de pessoas
+    let listaAmigos = document.getElementById("listaAmigos"); 
+    let item = document.createElement("li");                  
+    item.textContent = amigo;                                
+    listaAmigos.appendChild(item);                             
+
+    input.value = "";
+}
